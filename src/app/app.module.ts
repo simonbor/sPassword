41a5005f9487617002760a9c2d3219927f425c51
Simon Borsky <simonbor@gmail.com>
2021-01-11 22:24:55 +0200

    footer, text content fix, buy me a coffee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { CongratulationsComponent } from './congratulations/congratulations.component';
 import { HomeComponent } from './home/home.component';
 import { CredentialsComponent } from './credentials/credentials.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CongratulationsComponent,
     HomeComponent,
-    CredentialsComponent
+    CredentialsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
